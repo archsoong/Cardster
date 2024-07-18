b2b251455ffb1b70ba12f8ffd2621f236be92ecd
archsoong <39046143+archsoong@users.noreply.github.com>
2024-07-18 17:15:38 +0800

    Add Board, Cards, Mouse Event, Flip

--- src/script.js
+++ src/script.js
@@ -1,20 +1,14 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import GUI from 'lil-gui'
+import { gsap } from 'gsap'
 import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js'
 
-/**
- * Debug
- */
+
 const gui = new GUI()
 
-/**
- * Base
- */
-// Canvas
 const canvas = document.querySelector('canvas.webgl')
 
-// Scene
 const scene = new THREE.Scene()
 
 /**
@@ -25,9 +19,13 @@ const textureLoader = new THREE.TextureLoader()
 const cardBackTexture = textureLoader.load('./textures/card/dt_bg.jpg')
 const cardFrontTexture = textureLoader.load('./textures/card/dt_1.jpg')
 const cardAlphaTexture = textureLoader.load('./textures/card/alpha.jpg')
+const sceneForestTexture = textureLoader.load('./textures/scene/wood.jpg')
+const scenePlainTexture = textureLoader.load('./textures/scene/wood.jpg')
 
 cardBackTexture.colorSpace = THREE.SRGBColorSpace
 cardFrontTexture.colorSpace = THREE.SRGBColorSpace
+sceneForestTexture.colorSpace = THREE.SRGBColorSpace
+scenePlainTexture.colorSpace = THREE.SRGBColorSpace
 
 /**
  * Lights
@@ -35,155 +33,77 @@ cardFrontTexture.colorSpace = THREE.SRGBColorSpace
 const ambientLight = new THREE.AmbientLight(0xffffff, 1)
 scene.add(ambientLight)
 
-// const pointLight = new THREE.PointLight(0xffffff, 30)
-// pointLight.position.x = 2
-// pointLight.position.y = 3
-// pointLight.position.z = 4
-// scene.add(pointLight)
-
 
 /**
  * Objects
  */
+const sceneMaterial = new THREE.MeshBasicMaterial()
+sceneMaterial.map = sceneForestTexture
 
 const cardFrontMaterial = new THREE.MeshBasicMaterial()
 cardFrontMaterial.map = cardFrontTexture
 cardFrontMaterial.transparent = true
-cardFrontMaterial.opacity = 1
 cardFrontMaterial.alphaMap = cardAlphaTexture
 
 const cardBackMaterial = new THREE.MeshBasicMaterial()
 cardBackMaterial.map = cardBackTexture
 cardBackMaterial.transparent = true
-cardBackMaterial.opacity = 1
 cardBackMaterial.alphaMap = cardAlphaTexture
 
-// MeshNormalMaterial
-// const material = new THREE.MeshNormalMaterial()
-// material.flatShading = true
-
-// MeshMatcapMaterial
-// const material = new THREE.MeshMatcapMaterial()
-// material.matcap = matcapTexture
-
-// MeshDepthMaterial
-// const material = new THREE.MeshDepthMaterial()
-
-// MeshLambertMaterial
-// const material = new THREE.MeshLambertMaterial()
-
-// MeshPhongMaterial
-// const material = new THREE.MeshPhongMaterial()
-// material.shininess = 100
-// material.specular = new THREE.Color(0x1188ff)
-
-// MeshToonMaterial
-// const material = new THREE.MeshToonMaterial()
-// gradientTexture.minFilter = THREE.NearestFilter
-// gradientTexture.magFilter = THREE.NearestFilter
-// gradientTexture.generateMipmaps = false
-// material.gradientMap = gradientTexture
-
-// // MeshStandardMaterial
-// const material = new THREE.MeshStandardMaterial()
-// material.metalness = 1
-// material.roughness = 1
-// material.map = doorColorTexture
-// material.aoMap = doorAmbientOcclusionTexture
-// material.aoMapIntensity = 1
-// material.displacementMap = doorHeightTexture
-// material.displacementScale = 0.1
-// material.metalnessMap = doorMetalnessTexture
-// material.roughnessMap = doorRoughnessTexture
-// material.normalMap = doorNormalTexture
-// material.normalScale.set(0.5, 0.5)
-// material.transparent = true
-// material.alphaMap = doorAlphaTexture
-
-// gui.add(material, 'metalness').min(0).max(1).step(0.0001)
-// gui.add(material, 'roughness').min(0).max(1).step(0.0001)
-
-/**
- * MeshPhysicalMaterial
- */
-// Base material
-//const material = new THREE.MeshPhysicalMaterial()
-//material.metalness = 0
-//material.roughness = 0.15
-// material.map = doorColorTexture
-// material.aoMap = doorAmbientOcclusionTexture
-// material.aoMapIntensity = 1
-// material.displacementMap = doorHeightTexture
-// material.displacementScale = 0.1
-// material.metalnessMap = doorMetalnessTexture
-// material.roughnessMap = doorRoughnessTexture
-// material.normalMap = doorNormalTexture
-// material.normalScale.set(0.5, 0.5)
-// material.transparent = true
-// material.alphaMap = doorAlphaTexture
-
-//gui.add(material, 'metalness').min(0).max(1).step(0.0001)
-//gui.add(material, 'roughness').min(0).max(1).step(0.0001)
-
-// // Clearcoat
-// material.clearcoat = 1
-// material.clearcoatRoughness = 0
-
-// gui.add(material, 'clearcoat').min(0).max(1).step(0.0001)
-// gui.add(material, 'clearcoatRoughness').min(0).max(1).step(0.0001)
-
-// // Sheen
-// material.sheen = 1
-// material.sheenRoughness = 0.25
-// material.sheenColor.set(1, 1, 1)
-
-// gui.add(material, 'sheen').min(0).max(1).step(0.0001)
-// gui.add(material, 'sheenRoughness').min(0).max(1).step(0.0001)
-// gui.addColor(material, 'sheenColor')
-
-// // Iridescence
-// material.iridescence = 1
-// material.iridescenceIOR = 1
-// material.iridescenceThicknessRange = [ 100, 800 ]
-
-// gui.add(material, 'iridescence').min(0).max(1).step(0.0001)
-// gui.add(material, 'iridescenceIOR').min(1).max(2.333).step(0.0001)
-// gui.add(material.iridescenceThicknessRange, '0').min(1).max(1000).step(1)
-// gui.add(material.iridescenceThicknessRange, '1').min(1).max(1000).step(1)
-
-// Transmission
-//material.transmission = 1
-//material.ior = 1.5
-//material.thickness = 0.5
-
-//gui.add(material, 'transmission').min(0).max(1).step(0.0001)
-//gui.add(material, 'ior').min(1).max(10).step(0.0001)
-//gui.add(material, 'thickness').min(0).max(1).step(0.0001)
-
 // Objects
-const cardFront = new THREE.Mesh(
-    new THREE.PlaneGeometry(2, 3),
-    cardFrontMaterial
-)
-const cardBack = new THREE.Mesh(
-    new THREE.PlaneGeometry(2, 3),
-    cardBackMaterial
+const sceneBoard = new THREE.Mesh(
+    new THREE.PlaneGeometry(16, 12),
+    sceneMaterial
 )
 
-// Create a group and add both planes to it
-const card = new THREE.Group();
-card.add(cardFront);
-cardBack.rotation.y = Math.PI;
-card.add(cardBack);
+const rowSize = 4
+const colSize = 6
+const spacing = 0.2
+const cardWidth = 1.6
+const cardHeight = 2.4
+const spacingX = cardWidth + spacing
+const spacingY = cardHeight + spacing
 
-scene.add(card)
+const cards = [];
 
-gui.add(card.rotation,'z').min(0).max(1).step(0.01)
-gui.add(card.rotation,'x').min(0).max(1).step(0.01)
+const cardAxis = new THREE.AxesHelper( 5 );
 
+const planeGeometry = new THREE.PlaneGeometry(1.6, 2.4)
+
+for (let i = 0; i < rowSize; i++) {
+    for (let j = 0; j < colSize; j++) {
+
+        const cardFront = new THREE.Mesh(planeGeometry, cardFrontMaterial);
+        const cardBack = new THREE.Mesh(planeGeometry, cardBackMaterial);
+
+        const card = new THREE.Group();
+        card.add(cardFront);
+        cardBack.rotation.y = Math.PI;
+        card.add(cardBack);
+
+        card.position.z = 0.05;
+        card.position.x = j * spacingX - (colSize * spacingX / 2) + spacingX/2
+        card.position.y = i * spacingY - (rowSize * spacingY / 2) + spacingY/2
+        
+        cardFront.name = 'cardFront '+ i + ' ' + j
+        cardBack.name = 'cardBack '+ i + ' ' + j
+
+        card.name = 'card '+ i + ' ' + j
+
+        cards.push(card)
+        scene.add(card);
+    }
+}
+
+
+scene.add(sceneBoard)
+
+const clock = new THREE.Clock()
+const raycaster = new THREE.Raycaster()
 /**
  * Sizes
  */
+
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
@@ -208,13 +128,49 @@ window.addEventListener('resize', () =>
  * Camera
  */
 // Base camera
+const aspectRatio = sizes.width / sizes.height
+//const camera = new THREE.OrthographicCamera(- 1 * aspectRatio*5, 1 * aspectRatio*5, 5, -5, 1, 100)
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 1
-camera.position.y = 1
-camera.position.z = 3
+camera.position.x = 0
+camera.position.y = -4
+camera.position.z = 10
+
 scene.add(camera)
 
+const cameraGUI = gui.addFolder("Camera")
+cameraGUI.add(camera.position,'x').min(0).max(10).step(0.1)
+cameraGUI.add(camera.position,'y').min(-10).max(10).step(0.1)
+cameraGUI.add(camera.position,'z').min(0).max(10).step(0.1)
 
+const mouse = new THREE.Vector2()
+
+window.addEventListener('mousemove', (event) =>
+    {
+        mouse.x = event.clientX / sizes.width * 2 - 1
+        mouse.y = - (event.clientY / sizes.height) * 2 + 1
+    })
+
+
+window.addEventListener('click', () =>
+{
+    console.log(mouse)
+
+    if(currentIntersect)
+    {
+        console.log(currentIntersect.object.parent)
+
+        const flip = currentIntersect.object.parent.rotation.y == 0 ? Math.PI : 0
+
+        const timeline = gsap.timeline({ yoyo: true });
+        timeline
+            .to(currentIntersect.object.parent.position, { duration: 0.3, z: spacingX/2 })
+            .to(currentIntersect.object.parent.rotation, { duration: 0.3, y: flip})
+            .to(currentIntersect.object.parent.position, { duration: 0.3, z: 0.05 })
+        
+    }
+
+    currentIntersect = null
+})
 
 // Controls
 const controls = new OrbitControls(camera, canvas)
@@ -227,21 +183,26 @@ const renderer = new THREE.WebGLRenderer({
     antialias: true,
     canvas: canvas
 })
+
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 /**
  * Animate
  */
-const clock = new THREE.Clock()
+let currentIntersect = null
 
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
-
-    // Update objects
-    // card.rotation.y = 0.1 * elapsedTime
-
+    raycaster.setFromCamera(mouse, camera)
+    
+    const intersects = raycaster.intersectObjects(cards)
+    
+    if(intersects.length)
+    {
+        currentIntersect = intersects[0]
+    }
 
     // Update controls
     controls.update()
